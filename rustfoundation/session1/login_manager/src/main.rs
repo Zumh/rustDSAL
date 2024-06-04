d99c5fa9f694f0c8f3616dd5a86d194773589c0e
Zumh <17211423+Zumh@users.noreply.github.com>
2024-06-03 20:07:48 -0400

    update cli app

--- rustfoundation/session1/login_manager/src/main.rs
+++ rustfoundation/session1/login_manager/src/main.rs
@@ -1,4 +1,6 @@
 use clap::{Parser, Subcommand};
+use authentication::{get_users};
+
 
 #[derive(Parser)]
 #[command()]
@@ -12,12 +14,25 @@ enum Commands {
     /// List all users.
     List,
 }
+
+fn list_users(){
+    println!("{:<20}{:<20}", "Username", "Role");
+    println!("{:-<40}", "");
+
+
+    let users = get_users();
+
+    users.iter().for_each(|(_, user)|{
+        println!("{:<20}{:20?}", user.username, user.role);
+    });
+}
 fn main() {
     let cli = Args::parse();
 
     match cli.command{
         Some(Commands::List) => {
-            println!("List of users");
+            list_users();
+            //println!("List of users");
         }
         None => {
             println!("Run with --help to see instructions.");
